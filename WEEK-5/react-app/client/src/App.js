5aa295b752e3e24ee6a352dbcccaa36c47077fec
themysterysolver <arjunprabhakar1910@gmail.com>
2025-03-28 18:14:24 +0530

    FEAT:my first react code

---
+++ WEEK-5/react-app/client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let a="Hello world!"
+  return (
+    <div>
+      <h1>{a}</h1>
+    </div>
+  );
+}
+
+export default App;
