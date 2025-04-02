4543dc236c55c5849e8a26f0e9134a71d8742188
themysterysolver <arjunprabhakar1910@gmail.com>
2025-04-02 09:52:30 +0530

    FEAT:credit page done without more category

---
+++ WEEK-5/EXPENSE TRACKER/script/credit.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    document.querySelector("form").addEventListener("submit",(e)=>{
+        e.preventDefault();
+
+        let amount=parseFloat(document.getElementById("amount").value);
+        let category=document.getElementById("credit_drop").value;
+        let note=document.getElementById("note").value;
+        let type="Credit";
+        let new_balance=(parseFloat(localStorage.getItem("balance"))||0)+amount;
+
+        let new_transaction={
+            date:new Date().toLocaleString(),
+            type:type,
+            amount:amount,
+            balance:new_balance,
+            category:category,
+            desc:note
+        }
+        localStorage.setItem("balance",new_balance);
+
+        let transaction_hist=JSON.parse(localStorage.getItem("transaction"))||[];
+        transaction_hist.push(new_transaction);
+        localStorage.setItem("transactions",JSON.stringify(transaction_hist));
+
+        console.log(JSON.parse(localStorage.getItem("transactions")));
+        window.location.href = "balance.html";
+    });
+});
