d36e5df5f4ad0c4e8fcee1225b40dddb7026b270
themysterysolver <arjunprabhakar1910@gmail.com>
2025-04-02 10:46:45 +0530

    FEAT:transactions+forms style

--- WEEK-5/EXPENSE TRACKER/script/transaction.js
+++ WEEK-5/EXPENSE TRACKER/script/transaction.js
@@ -5,5 +5,32 @@ document.addEventListener("DOMContentLoaded",()=>{
     console.log(JSON.parse(transactions));
     if(!transactions||JSON.parse(transactions).length===0){
         trans.innerHTML=`<h3>No Transactions made yet!</h3>`
+    }else{
+        let table=`
+        <table class="table table-striped table-secondary table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">Date</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Amount</th>
+                    <th scope="col">Balance</th>
+                    <th scope="col">Category</th>
+                    <th scope="col">Description</th>
+                </tr>
+        </thead><tbody>`;
+
+        JSON.parse(transactions).reverse().forEach(el => {
+            table+=`
+                <tr>
+                    <td>${el.date}</td>
+                    <td>${el.type}</td>
+                    <td>${el.amount}</td>
+                    <td>${el.balance}</td>
+                    <td>${el.category}</td>
+                    <td>${el.desc}</td>
+                </tr>`
+        });
+        table+=`</tbody></table>`
+        trans.innerHTML=table;
     }
 });
