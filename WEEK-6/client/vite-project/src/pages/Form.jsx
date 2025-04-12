6d43928608e1b6b22f857a0108b0b8e30e8de7c8
themysterysolver <arjunprabhakar1910@gmail.com>
2025-04-12 09:23:15 +0530

    FEAT:useEffect and 3 mounting phases

--- WEEK-6/client/vite-project/src/pages/Form.jsx
+++ WEEK-6/client/vite-project/src/pages/Form.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { useState } from 'react';
+import { useEffect } from 'react'
 
 const Form = () => {
+    useEffect(()=>{
+                return(()=>{console.log("Form:only executed when page is chnaged!")})
+            },[]);
+    useEffect(()=>{
+        console.log("Only executed while mounting!");
+    },[]);
+    
+    let [c1,setC1]=useState(0);
+    let [c2,setC2]=useState(0);
+    useEffect(()=>{
+        console.log("I am executed only when c1 is changed!")
+    },[c1])
   return (
     <div>
       <form>
         <label>NAME</label><input type="text" placeholder="xxx"/>
       </form>
+      <button onClick={()=>{setC1(c1+1)}}>C1:{c1}</button>
+      <button onClick={()=>{setC2(c2+1)}}>C2:{c2}</button>
     </div>
   )
 }
--- WEEK-6/client/vite-project/src/pages/Home.jsx
+++ WEEK-6/client/vite-project/src/pages/Home.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState,useEffect } from 'react';
+import { useNavigate} from 'react-router-dom';
 
 
 const Home = () => {
+    useEffect(()=>{
+            return(()=>{console.log("HOME:only executed when page is chnaged!")})
+        },[]);
   let [count,setCount]=useState(0); 
   const nav=useNavigate();
   let move=()=>{
