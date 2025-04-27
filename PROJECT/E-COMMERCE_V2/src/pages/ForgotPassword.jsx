ea94922550332481d7aabe5ba023c85e0600187e
themysterysolver <arjunprabhakar1910@gmail.com>
2025-04-27 23:22:39 +0530

    FEAT:forgot password

--- PROJECT/E-COMMERCE_V2/src/pages/ForgotPassword.jsx
+++ PROJECT/E-COMMERCE_V2/src/pages/ForgotPassword.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const ForgotPassword = () => {
   return (
-    <div>
-      <h1>ForgotPassword</h1>
+    <div className="container">
+      <h2 className="text-center m-4">Forgot Password</h2>
+      <div className="text-center alert alert-info p-2">
+        <p>If you've forgotten your password, please contact us at <strong>shopify@example.com</strong> to reset your password.</p>
+      </div>
     </div>
   )
 }
