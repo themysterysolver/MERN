e96c0101af6a6fcf3801afda92cb2c242bdfaf4f
themysterysolver <arjunprabhakar1910@gmail.com>
2025-04-27 23:30:49 +0530

    FEAT:login updated forgot password

--- PROJECT/E-COMMERCE_V2/src/pages/Login.jsx
+++ PROJECT/E-COMMERCE_V2/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import {loginUser} from '../utils/localStorageUtils'
+import {Link } from 'react-router-dom';
 const Login = () => {
   const nav=useNavigate()
   const [username,setUsername]=useState('');
@@ -17,17 +18,23 @@ const Login = () => {
     }
   }
   return (
-    <div className="container my-4">
-      <h2 className="text-center mb-4">Signin</h2>
-      <div className="form-group mb-3">
-        <input className="form-control" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-      </div>
-      <div className="form-group mb-3">
-        <input className="form-control" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </div>
-      {error && <div className="alert alert-danger">{error}</div>}
-      <button className="btn btn-primary w-100" onClick={handleLogin}>Signin</button>
-    </div>
+    <>
+        <div className="container my-4">
+          <h2 className="text-center mb-4">Signin</h2>
+          <div className="form-group mb-3">
+            <input className="form-control" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+          </div>
+          <div className="form-group mb-3">
+            <input className="form-control" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          {error && <div className="alert alert-danger">{error}</div>}
+          <button className="btn btn-primary w-100" onClick={handleLogin}>Signin</button>
+          <div className="text-end mt-2">
+            <Link className="nav-link text-primary text-decoration-underline" to="/forgotpassword">Forgot password</Link>
+          </div>
+        </div>
+        
+    </>
   )
 }
 
