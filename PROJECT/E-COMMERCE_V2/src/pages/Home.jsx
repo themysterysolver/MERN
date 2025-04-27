73fd8174b542c669c3dea240a0f6ceb297712c68
themysterysolver <arjunprabhakar1910@gmail.com>
2025-04-27 22:27:25 +0530

    FEAT:home page done with fake api

--- PROJECT/E-COMMERCE_V2/src/pages/Home.jsx
+++ PROJECT/E-COMMERCE_V2/src/pages/Home.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
-
+import { addToCart } from '../utils/localStorageUtils';
+import {useEffect,useState} from 'react'
 const Home = () => {
+  const [products,setProducts]=useState([]);
+
+  useEffect(()=>{
+    fetch('https://fakestoreapi.com/products').then(res=>res.json()).then(json=>{setProducts(json)});
+  },[]);
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
+    <div className="container my-4">
+      <h2 className="text-center mb-4">Products</h2>
+      <div className="row">
+        {products.map(prod => (
+          <div key={prod.id} className="col-md-3 mb-4">
+            <div className="card h-100">
+              <img 
+                src={prod.image} 
+                alt={prod.title} 
+                className="card-img-top" 
+                style={{ objectFit: 'contain', height: '200px' }}/>
+              <div className="card-body d-flex flex-column">
+                <h5 className="card-title" style={{ fontSize: '1rem' }}>{prod.title}</h5>
+                <p className="card-text fw-bold">${prod.price}</p>
+                <button className="btn btn-primary mt-auto" 
+                  onClick={() => addToCart({ productId: prod.id, name: prod.title, price: prod.price, quantity: 1 })}>
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+  </div>
+);
+  
 }
 
 export default Home
