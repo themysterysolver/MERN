fc002b9979424761464da531b191948520abbc4f
themysterysolver <arjunprabhakar1910@gmail.com>
2025-08-20 15:28:12 +0530

    FEAT:templating

--- EXPRESS/WORKING/index.js
+++ EXPRESS/WORKING/index.js
@@ -1,40 +1,20 @@
+//embedded js - ejs
 import express from 'express'
 
 const app = express()
 const PORT = 3000
 
+// set EJS as view engine
+app.set('view engine','ejs')
+
 app.use(express.json());
 
 
-app.use((req,res,next)=>{
-    console.log('Middleware 1');
-    res.on('finish',()=>{
-        console.log('End!')
-    })
-    next()
-})
-
-app.use((req,res,next)=>{
-    console.log('Middleware 2');
-    res.on('finish',()=>{
-        console.log('End!2')
-    })
-    next()
-})
-
 app.get('/',(req,res)=>{
-    res.send('Hello!');
+    const username = 'Arjun'
+    res.render('index',{username});
 })
 
-app.get('/error',()=>{
-    throw new Error('Test error')
-})
-
-app.use((err,req,res,next)=>{
-    console.error(err.message)
-    res.send(err.message);
-    next()
-})
 
 app.listen(PORT,()=>{
     console.log(`server is running on http://localhost:${PORT}`); //template literal
