e12be1f2efd9f5310fdcb8170ea3c8ad99fe7d16
themysterysolver <arjunprabhakar1910@gmail.com>
2025-08-20 13:36:13 +0530

    FEAT:dynamic routing and query

--- EXPRESS/WORKING/index.js
+++ EXPRESS/WORKING/index.js
@@ -6,6 +6,23 @@ app.get('/',(req,res)=>{
     res.send('Hello World! Arjun!');
 }) 
 
+//about
+app.get('/about',(req,res)=>{
+    res.send('About message!');
+}) 
+
+//dynamic routing
+app.get('/user/:username',(req,res)=>{
+    const username = req.params.username;
+    res.send(`Welcome home ${username}`);
+});
+
+//querying string
+app.get('/search',(req,res)=>{
+    const keyword = req.query.keyword;
+    res.send(`searching for ${keyword}`);
+}); //http://localhost:3000/search?keyword=abi
+
 app.listen(PORT,()=>{
     console.log(`server is running on http://localhost:${PORT}`); //template literal
 }) //port,callback
