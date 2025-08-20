bcdf4f7528b8dec18010933ee1227eb0f71f3e50
themysterysolver <arjunprabhakar1910@gmail.com>
2025-08-20 15:21:10 +0530

    FEAT:error handling in  middleware

--- EXPRESS/WORKING/index.js
+++ EXPRESS/WORKING/index.js
@@ -6,20 +6,35 @@ const PORT = 3000
 app.use(express.json());
 
 
-app.use('/welcome',(req,res,next)=>{
-    console.log('A new req recieved at'+Date.now());
+app.use((req,res,next)=>{
+    console.log('Middleware 1');
+    res.on('finish',()=>{
+        console.log('End!')
+    })
+    next()
+})
+
+app.use((req,res,next)=>{
+    console.log('Middleware 2');
+    res.on('finish',()=>{
+        console.log('End!2')
+    })
     next()
 })
 
 app.get('/',(req,res)=>{
-    res.send('Hello World! Arjun!');
-}) 
+    res.send('Hello!');
+})
 
-app.get('/welcome',(req,res)=>{
-    res.send('Welcome! Arjun!');
-}) 
+app.get('/error',()=>{
+    throw new Error('Test error')
+})
 
-
+app.use((err,req,res,next)=>{
+    console.error(err.message)
+    res.send(err.message);
+    next()
+})
 
 app.listen(PORT,()=>{
     console.log(`server is running on http://localhost:${PORT}`); //template literal
