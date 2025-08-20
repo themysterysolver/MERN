3cb46455e28a5dc8c913fc54537ffe3b4d96a6b7
themysterysolver <arjunprabhakar1910@gmail.com>
2025-08-20 14:09:18 +0530

    FEAT:get,post,put

--- EXPRESS/WORKING/index.js
+++ EXPRESS/WORKING/index.js
@@ -18,6 +18,22 @@ app.get('/user/:username',userController);
 //querying string
 app.get('/search',searchController); //http://localhost:3000/search?keyword=abi
 
+app.post('/user', express.json(),(req,res)=>{
+    const { name, email } = req.body;
+    res.json({
+         message:  `User is ${name} with email ${email}`
+    })
+});
+
+app.put('/user/:abc', express.json(),(req,res)=>{
+    const uname = req.params.abc;
+    const { name, email } = req.body;
+    res.json({
+         message:  `User ${uname} is updates to ${name} with email ${email}`
+    })
+});
+
+
 app.listen(PORT,()=>{
     console.log(`server is running on http://localhost:${PORT}`); //template literal
 }) //port,callback
